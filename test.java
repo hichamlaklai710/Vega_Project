6b902c49b8c5aa0e718f0b6724db096fce8d464e
hichamlaklai710 <hicham_laklai@um5.ac.ma>
2022-03-25 17:10:10 +0100

    second changing

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Salam 3alaykoum\n");
-        System.out.println("wa 3alaykom salam ech ech ech");
+        System.out.println("wa 3alaykom salam hahaha");
     }
 }
