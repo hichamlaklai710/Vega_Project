f3b1da7d4c3bbf1e25c12763561a9ade107f6367
hichamlaklai710 <hicham_laklai@um5.ac.ma>
2022-03-25 17:20:05 +0100

    first changing

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Salam 3alaykoum\n");
-        System.out.println("wa 3alaykom salam hahaha");
+        System.out.println("wa 3alaykom salam iiiiiii");
     }
 }
